483678703cc133e0a645a374d344211791e1a43a
Mahbubul Kabir Farajee <mahbubul.kabir@northsouth.edu>
2021-03-27 01:50:07 +0600

    Signup , Login

---
+++ PHP Added Part/php_actions/login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'dbconnect.php';
+
+if (isset($_POST['submit'])) {
+
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $encrptpass = md5($pass);
+
+    $login = "SELECT user_id FROM user WHERE email = '$email' and password = '$encrptpass'";
+
+    $result = mysqli_query($db, $login);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $id = $row['user_id'];
+
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+       
+        $_SESSION['login_user'] = $id;
+        header("location: ../home.php");
+    } else {
+        $error = "Your Login Name or Password is invalid";
+    }
+}
